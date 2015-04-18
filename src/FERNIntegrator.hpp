a2e8e52e6b38eddc54a7eb5c952e04a3ed53d5d1
Benjamin Brock <brock@utk.edu>
2015-04-18 15:37:25 -0400

    Removed GPU code from FERNIntegrator.

--- src/FERNIntegrator.hpp
+++ src/FERNIntegrator.hpp
@@ -19,25 +19,11 @@ public:
 	FERNIntegrator();
 	~FERNIntegrator();
 	
-	// void loadProperties(const char *filename);
-	
-	void initializeCuda();
-	void prepareKernel();
-	
 	/**	Launches a kernel to process the IntegrationData
 	*/
 	void integrate(IntegrationData &integrationData);
-	static void checkCudaErrors();
 	
 	Network network;
-	
-private:
-	Network devNetwork;
-	
-	dim3 blocks;
-	dim3 threads;
-	
-	static void devcheck(int gpudevice);
 };
 
 #endif
