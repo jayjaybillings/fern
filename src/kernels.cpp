24fd65734b4799f1cd8a91a9478c0b8a0ea6d12c
Jay Jay Billings <billingsjj@ornl.gov>
2015-10-06 17:38:57 +0000

    Fixed test failure from previous commit.
    The abs function does not work exactly as advertised in the C++11
    documentation. I restored and refactored kernels again to find the bug.
    Everything is refactored up to its previous state, minus the abs()
    calls, which I'll look at next.
    
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

--- src/kernels.cpp
+++ src/kernels.cpp
@@ -1,34 +1,3 @@
-/**----------------------------------------------------------------------------
-Copyright (c) 2015-, The University of Tennessee
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* Neither the name of fern nor the names of its
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-Author(s): Jay Jay Billings, Ben Brock, Andrew Belt, Dan Shyles, Mike Guidry
------------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <cmath>
 #include "kernels.hpp"
@@ -107,6 +76,20 @@ static Globals * globals;
 
 fern_real *Y;
 
+
+/**
+ * This operation returns the absolute value of a fern_real.
+ * @param val the number for which the absolute value should be found
+ * @return the absolute value of val
+ */
+fern_real abs(fern_real val) {
+#ifdef FERN_SINGLE
+	return fabsf(val);
+#else
+	return fabs(val);
+#endif
+}
+
 /**
  * This function checks the status for the plotting
  */
@@ -158,9 +141,9 @@ void checkPlotStatus(fern_real time, fern_real stepSize, fern_real maxTime, fern
 }
 
 void initialize(Network * networkInfo, IntegrationData * data,
-		Globals * globalsPtr) {
-	globals = globalsPtr;
+		Globals *globalsPtr) {
 	network = networkInfo;
+	globals = globalsPtr;
 
 	numberSpecies = network->species;
 	numberReactions = network->reactions;
@@ -207,16 +190,14 @@ void initialize(Network * networkInfo, IntegrationData * data,
 	FplusMax = network->FplusMax;
 	FminusMax = network->FminusMax;
 
+	return;
+}
+
+void integrate() {
 	/* Assign IntegrationData pointers. */
 
 	Y = integrationData->Y;
 
-}
-
-/**
- * This function performs the time integration of the network->
- */
-void integrate() {
 	fern_real maxFlux;
 	fern_real sumX;
 	fern_real t;
